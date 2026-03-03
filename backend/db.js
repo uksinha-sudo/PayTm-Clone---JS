bef0b4d5465a07387f4cef8fa1b5805454edac67
uksinha-sudo <sinhanikhil377@gmail.com>
2026-03-03 16:08:19 +0530

    Introduced account Schema in Database

--- backend/db.js
+++ backend/db.js
@@ -7,9 +7,24 @@ const userSchema = mongoose.Schema({
     password: {type: String, length: 18}
 });
 
+const accountSchema = mongoose.Schema({
+    balance: {type: Number, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
+})
+// NOTE FOR Storing balance in DB => In real world, we should never store `floats` for balances in the database.
+// You usually store a n integer which represents which the INR value with 
+// decimal places (for eg, if someone has 33.33 rs in their account,
+// you store 3333 in the database
+
+// There is a certain precision that you need to support (which for india is 
+// 2/4 decimal places) and this allows you to get rid of precision
+// error by storing integers in your DB
+
 
 const userModel = mongoose.model("User", userSchema);
+const accountModel = mongoose.model("Account", accountSchema);
 
 module.exports = {
-    userModel
+    userModel, 
+    accountModel
 }
